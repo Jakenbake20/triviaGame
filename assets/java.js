a0d4b4720661e430cc62bd8380211f8f8500262f
Jakenbake <jacob_wheat@yahoo.com>
2020-03-30 20:21:58 -0600

    added more java and implemented some es6.

--- assets/java.js
+++ assets/java.js
@@ -3,22 +3,54 @@ var quizQuestions = [
         question: "At the beginning of the series, how many children did Ned and Catelyn Stark have?",
         choices:["Two", "Three", "Five", "Twelve"],
         correctAnswer: "Five"
-    }
+    },
 
     {
         question: "Who was the first person in the series to be called King of the Nort",
         choices:["Robb Stark", "Bran Stark", "Edmure Tully", "Hodor"],
         correctAnswer: "Robb Stark"
-    }
+    },
 
     {
         question: "How does Daenerys hatch her dragon eggs?",
         choices:["A funeral pyre", "She sits on them", "She throws them off a cliff", "She makes scrambled eggs with them"],
         correctAnswer: "A funeral pyre"
     }
-
-
-
-
 ];
 
+//Initial values
+var counter = 30;
+var currentQuestion = 0;
+var scrore = 0;
+var lost = 0;
+var timer;
+
+//Display the queston and the choices in the browser
+function loadQuestion(){
+    
+    var question = quizQuestions[currentQuestion].question;
+    var choices =  quizQuestions[currentQuestion].choices;
+
+    $('#timer').html('Timer: '+ counter);
+    $('#game').html('
+    <h4>' + question + '</h4>
+    ');
+    // $('#answers').html('<p>' + $(loadChoices) + '</p>')
+}    
+        
+    
+ 
+
+
+function loadChoices(choices) {
+    var result = '';
+
+    for (var i = 0; i < choices.length; i++){
+        result += '<p class ="choice" data-answer="${choices[i]}">${choices[i]}</p>';
+    }
+
+    return result;
+}
+
+
+loadQuestion();
